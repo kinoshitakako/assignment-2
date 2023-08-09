f219bc817f8a3da8db028f5353e23216a1ac828b
KakoKinoshita <140261493+kinoshitakako@users.noreply.github.com>
2023-08-09 00:53:40 +0000

    [Start]課題2booker

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(books.id)  end
+
+end
