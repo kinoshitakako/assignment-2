787a0a95cb5da8e7a68abf60b7034c223e9e4398
KakoKinoshita <140261493+kinoshitakako@users.noreply.github.com>
2023-08-10 07:15:51 +0000

    [Finish]課題2booker

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,15 +4,51 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
+  def index
     @book = Book.new
+    @books = Book.all
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id) , notice: 'Book was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @book = Book.new(book_params)
+    # @book.save
+    # redirect_to book_path(@book.id)
+    @books = Book.all
+    
+    if @book.save
+      redirect_to book_path(@book.id) , notice: 'Book was successfully created'
+    else
+       @books = Book.all
+      render :index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(books.id)  end
-
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
+
+
+
